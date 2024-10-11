75fb998dc6c578917e22986cfbb3cd3de15bee3e
AK-Rasel <raselak101010@gmail.com>
2024-10-11 11:29:06 +0600

    only stayle and how to data puse column ,card data

---
+++ src/App.jsx
@@ -0,0 +1,198 @@
+import { useState } from "react";
+import { FaFire } from "react-icons/fa";
+import { FiPlus, FiTrash } from "react-icons/fi";
+export const App = () => {
+  return (
+    <div className="h-screen w-full bg-neutral-900 text-neutral-50">
+      <Board />
+    </div>
+  );
+};
+// full display
+const Board = () => {
+  const [cards, setCards] = useState(DEFAULT_CARDS);
+  return (
+    <div className="flex h-full w-full gap-3 overflow-scroll p-12">
+      <Column
+        title="Backlog"
+        headingColor="text-neutral-500"
+        column="backlog"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="TODO"
+        headingColor="text-yellow-200"
+        column="todo"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="In progress"
+        headingColor="text-blue-200"
+        column="doing"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="Complete"
+        headingColor="text-emerald-200"
+        column="done"
+        cards={cards}
+        setCards={setCards}
+      />
+      <BurnCard setCards={setCards} />
+    </div>
+  );
+};
+// section
+const Column = ({ title, headingColor, column, cards, setCards }) => {
+  const [active, setActive] = useState(false);
+  const filteredCards = cards.filter((c) => c.column === column);
+  return (
+    <div className="w-56 shrink-0">
+      <div className="mb-3 flex items-center justify-between">
+        <h3 className={`font-medium ${headingColor}`}>
+          <span className="rounded mr-4 text-sm text-neutral-400">
+            {filteredCards.length}
+          </span>
+          {title}
+        </h3>
+      </div>
+      <div
+        className={`h-full w-full transition-colors ${
+          active ? "bg-neutral-800/50" : "bg-neutral-800/0"
+        }`}
+      >
+        {filteredCards.map((c) => {
+          return <Card key={c.id} {...c} />;
+        })}
+        <DropInDicator beforeId="-1" column={column} />
+        <AddCard column={column} setCards={setCards} />
+      </div>
+    </div>
+  );
+};
+// card
+const Card = ({ title, id, column }) => {
+  return (
+    <>
+      <DropInDicator beforeId={id} column={column} />
+      <div
+        draggable="true"
+        className="cursor-grab rounded border border-neutral-700 bg-neutral-800 p-3 active:cursor-grabbing"
+      >
+        <p className="text-sm text-neutral-100">{title}</p>
+      </div>
+    </>
+  );
+};
+
+const DropInDicator = ({ beforeId, column }) => {
+  return (
+    <div
+      data-before={beforeId || "-1"}
+      data-column={column}
+      className="my-0.5 h-0.5 w-full bg-violet-400 opacity-0"
+    ></div>
+  );
+};
+
+const BurnCard = ({ setCards }) => {
+  const [active, setActive] = useState(false);
+  return (
+    <div
+      className={`mt-10 grid h-56 w-56 shrink-0 place-content-center rounded border text-3xl ${
+        active
+          ? "border-red-800 bd-red-800/20 text-red-500 "
+          : "border-neutral-500 bg-neutral-500/20 text-neutral-500"
+      }`}
+    >
+      {active ? <FaFire className="animate-bounce" /> : <FiTrash />}
+    </div>
+  );
+};
+
+const AddCard = ({ column, setCards }) => {
+  const [text, setText] = useState("");
+  const [adding, setAdding] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(text);
+    if (!text.trim().length) return;
+    const newCard = {
+      column,
+      title: text.trim(),
+      id: Math.random().toString(),
+    };
+    setCards((pv) => [...pv, newCard]);
+    setAdding(false);
+  };
+  return (
+    <>
+      {adding ? (
+        <form onSubmit={handleSubmit}>
+          <textarea
+            onChange={(e) => setText(e.target.value)}
+            autoFocus
+            placeholder="add new task..."
+            className="w-full rounded border-violet-400 bg-violet-400/20 p-3 text-sm text-neutral-50 placeholder-violet-300 focus:outline-0"
+          />
+          <div className="mt-1.5 flex items-center justify-end gap-1.5">
+            <button
+              onClick={() => setAdding(false)}
+              className="px-3 py-1.5 text-xs text-neutral-400 transition-colors hover:text-neutral-50"
+            >
+              Close
+            </button>
+            <button
+              type="submit"
+              className="flex items-center gap-1.5 rounded bg-neutral-50 px-3 py-1.5 text-xs text-neutral-950 transition-colors hover:bg-neutral-300"
+            >
+              <span> Add</span>
+              <FiPlus />
+            </button>
+          </div>
+        </form>
+      ) : (
+        <button
+          onClick={() => setAdding(true)}
+          className="flex w-full items-center gap-1.5 px-3 py-1.5 text-xs text-neutral-400 transition-colors hover:text-neutral-50"
+        >
+          <span>Add card</span>
+          <FiPlus />
+        </button>
+      )}
+    </>
+  );
+};
+
+const DEFAULT_CARDS = [
+  // BACKLOG
+  { title: "Look into render bug in dashboard", id: "1", column: "backlog" },
+  { title: "SOX compliance checklist", id: "2", column: "backlog" },
+  { title: "[SPIKE] Migrate to Azure", id: "3", column: "backlog" },
+  { title: "Document Notifications service", id: "4", column: "backlog" },
+  // TO do
+  {
+    title: "Research DB options for new microservice",
+    id: "5",
+    column: "todo",
+  },
+  { title: "Postmortem for outage", id: "6", column: "todo" },
+  { title: "Sync with product on Q3 roadmap", id: "7", column: "todo" },
+
+  // DOING
+  {
+    title: "Refactor context providers to use Zustand",
+    id: "8",
+    column: "doing",
+  },
+  { title: "Add logging to daily CRON", id: "9", column: "doing" },
+  // DONE
+  {
+    title: "Set up DD dashboards for Lambda listener",
+    id: "10",
+    column: "done",
+  },
+];
